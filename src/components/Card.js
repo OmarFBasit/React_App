1306f712ffd5c35e134a96886b5fe5a7c2169460
Omar Faruk <Omarfaruk885@gmail.com>
2022-09-23 01:07:46 +0600

    Working with destructring

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
-const con="Card Note";
 const date=new Date();
 const currentYear=date.getFullYear();
 const todaydate=date.getDate();
 const todaymonth=date.getMonth();
 
 function Card(props){
+    const {titletex,description}=props;
 return  <div className='card'>
-<h3 className='cardtitle'>{props.titletex}</h3>
-<p className='cardDescription'>{props.description}</p>
+<h3 className='cardtitle'>{titletex}</h3>
+<p className='cardDescription'>{description}</p>
 <p className='cardfooter'>{todaydate}/{todaymonth}/{currentYear} </p>
 </div> 
 }
