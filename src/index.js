aaab1aa4c87f86407c284e8aeb54bf4b3dccc68c
Omar Faruk <Omarfaruk885@gmail.com>
2022-09-22 22:14:43 +0600

    CSS ADD via StyleSheet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'
 
 const Wel="Wellcome User";
 const con="Contact";
@@ -7,21 +8,21 @@ const date=new Date();
 const currentYear=date.getFullYear();
 const todaydate=date.getDate();
 const todaymonth=date.getMonth();
-const headingstyle = {  
-  color: "white",  
-  backgroundColor: "purple",  
-  padding: "15px",  
-  fontFamily: "Arial",
-  textAlign: "center",
-  fontSize: "3rem" 
-};
+// const headingstyle = {  
+//   color: "white",  
+//   backgroundColor: "purple",  
+//   padding: "15px",  
+//   fontFamily: "Arial",
+//   textAlign: "center",
+//   fontSize: "3rem" 
+// };
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
  
-    <h1 style={headingstyle}>{Wel}</h1>
+    <h1 className='headingstyle'>{Wel}</h1>
     <h3>{con}</h3>
     <p>{todaydate} - {todaymonth} -{currentYear} </p>
     </div>
