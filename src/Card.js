44b04f2b09cb779d3a12b8e6f6e9e439ad68ac3b
Omar Faruk <Omarfaruk885@gmail.com>
2022-09-23 00:20:18 +0600

    Card Component Add using Extra File

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+const Wel="Wellcome User";
+const con="Card Note";
+const date=new Date();
+const currentYear=date.getFullYear();
+const todaydate=date.getDate();
+const todaymonth=date.getMonth();
+
+function Card(){
+return <div className='card'>
+<h3 className='cardtitle'>{con}</h3>
+<p className='cardDescription'>Hoping for a better future. One day I can achieve sure.</p>
+<p className='cardfooter'>{todaydate}/{todaymonth}/{currentYear} </p>
+</div>
+
+}
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
+import Card from './Card.js';
+
 
 const Wel="Wellcome User";
-const con="Card Note";
-const date=new Date();
-const currentYear=date.getFullYear();
-const todaydate=date.getDate();
-const todaymonth=date.getMonth();
-
-function Card(){
-return <div className='card'>
-<h3 className='cardtitle'>{con}</h3>
-<p className='cardDescription'>Hoping for a better future. One day I can achieve sure.</p>
-<p className='cardfooter'>{todaydate}/{todaymonth}/{currentYear} </p>
-</div>
-
-}
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -25,19 +12,10 @@ root.render(
   <div>
  
     <h1 className='headingstyle'>{Wel}</h1>
-
-    <div className='card'>
-    <h3 className='cardtitle'>{con}</h3>
-    <p className='cardDescription'>Hoping for a better future. One day I can achieve sure.</p>
-    <p className='cardfooter'>{todaydate}/{todaymonth}/{currentYear} </p>
-    </div>
-
     <Card />
     <Card />
     <Card />
-
-    
-
+    <Card />
     </div>
 
 );
