5556baf9ad5319bc16a6a10fb89c0a06fd07a816
Omar Faruk <Omarfaruk885@gmail.com>
2022-09-22 21:16:37 +0600

    Set date and some variable

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const Wel="Wellcome User";
+const con="Contact";
+const date=new Date();
+const currentYear=date.getFullYear();
+const todaydate=date.getDate();
+const todaymonth=date.getMonth();
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <div>
  
-    <h1>wellcome Omar</h1>
+    <h1>{Wel}</h1>
+    <h3>{con}</h3>
+    <p>{todaydate}- {todaymonth} -{currentYear} </p>
+    </div>
 
 );
-
 
 
 
