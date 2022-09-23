eabb4a1c521b267b8c280ea5912eb64e92cfba82
Omar Faruk <Omarfaruk885@gmail.com>
2022-09-23 15:55:03 +0600

    Data mapping set

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import Card from './components/Card.js';
 import Data from './data.json'
 
 let items=[];
-for(let x=0;x<Data.length;x++){
-    items.push(<Card titletex={Data[x].title} description={Data[x].Description}/>)
-}
+items=Data.map((items,index)=> <Card key={index} titletex={items.title} description={items.Description}/>)
 
 const Wel="Wellcome User";
 function App(){
