90b05f4e92ed96023ae0e3a3357c12f9c2a1a942
Omar Faruk <Omarfaruk885@gmail.com>
2022-09-23 14:24:02 +0600

    create json file and pass the data Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Card from './components/Card.js';
+import Data from './data.json'
+
 const Wel="Wellcome User";
 function App(){
     return <div>
         <h1 className='headingstyle'>{Wel}</h1>
-        <Card titletex="Card Note 1" description="Inshall one day i can achieve"/>
-        <Card titletex="Card Note 2" description="Omar Can Do this"/>
-        <Card titletex="Card Note 3" description="Hello world"/>
+        <Card titletex={Data[0].title} description={Data[0].Description}/>
+        <Card titletex={Data[1].title} description={Data[1].Description}/>
+        <Card titletex={Data[2].title} description={Data[2].Description}/>
     </div>
 
 }
